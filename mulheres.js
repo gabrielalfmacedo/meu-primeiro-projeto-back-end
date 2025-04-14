c55d1c2fd20ee24ea6c5792a4a474f2b92b3032b
gabrielalfmacedo <gabrielalfmacedo@gmail.com>
2025-04-14 20:16:21 -0300

    adiciona a primeira versao do meu projeto

---
+++ mulheres.js
@@ -0,0 +1,40 @@
+const express = require("express")
+const router = express.Router()
+
+
+const app = express()
+const porta = 3333
+
+
+const mulheres = [
+    {
+        nome: 'Gabriela Macedo',
+        imagem:'https://github.com/simaraconceicao.png',
+        minibio: 'Desenvolvedora e instrutora',
+
+    },
+
+    {
+        nome:'Iana Chan',
+        imagem:'https://bit.ly/3JCXBqP',
+        minibio:'Fundadora da Programaria',
+    },
+
+    {
+        nome: 'Nina da hora',
+        imagem:'https://bit.ly/3FKpFaz',
+        minibio: 'hacker antirracista',
+    }
+]
+
+function mostraMulheres(request, response) {
+    response.json(mulheres)
+
+    }
+
+function mostraPorta() {
+    console.log("Servidor criado e rodando na porta ", porta)
+}
+
+app.use(router.get('/mulheres', mostraMulheres))
+app.listen(porta, mostraPorta)
